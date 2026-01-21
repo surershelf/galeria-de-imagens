57a9199a800eedcc74d8ec636a7017c54c4a573f
mazzo <mazzolucas0@gmail.com>
2026-01-21 10:08:37 -0300

    Projeto de Galeria

---
+++ backend/Galeria/src/main/java/com/mazzo/Galeria/GaleriaApplication.java
@@ -0,0 +1,13 @@
+package com.mazzo.Galeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaApplication.class, args);
+	}
+
+}
---
+++ backend/Galeria/src/test/java/com/mazzo/Galeria/GaleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazzo.Galeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
