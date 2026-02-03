57544f84313c6654d0118968594d4740b3b3c0b1
mazzo <mazzolucas0@gmail.com>
2026-02-03 15:51:52 -0300

    Terminando API

--- backend/Galeria/src/main/java/com/mazzo/Galeria/controller/ImagemController.java
+++ backend/Galeria/src/main/java/com/mazzo/Galeria/controller/ImagemController.java
@@ -2,11 +2,8 @@ package com.mazzo.Galeria.controller;
 
 import com.mazzo.Galeria.model.Imagem;
 import com.mazzo.Galeria.repository.ImagemRepository;
-import jakarta.websocket.server.PathParam;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,13 +25,16 @@ public class ImagemController {
 
     record AddImagemResponse(UUID id, String title, String description){}
 
+    record AttTitleAndDescriptionRequest(String title, String description){}
+
+
     @PostMapping(path = "add")
     public ResponseEntity<?> addImagem(@RequestParam("file") MultipartFile file,
                                        @RequestParam("title") String title,
                                        @RequestParam("description") String description)
     {
         try {
-            if (file.isEmpty() || title.isEmpty()) {
+            if (file.isEmpty() || title == null || title.isEmpty()) {
                 return ResponseEntity.badRequest()
                         .body(new ErrorResponse("Título ou imagem não enviada!"));
             }
@@ -62,9 +62,8 @@ public class ImagemController {
             return  ResponseEntity.status(HttpStatus.CREATED)
                     .body(new AddImagemResponse(savedImage.getId(),savedImage.getTitle(), savedImage.getDescription()));
         } catch (Exception e){
-            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ErrorResponse(e.getMessage()));
+                    .body(new ErrorResponse("Erro interno do servidor"));
         }
 
 
@@ -87,6 +86,74 @@ public class ImagemController {
         try{
             List<Imagem> images = imagemRepository.findAllActive();
             return  ResponseEntity.status(HttpStatus.OK).body(images);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("Erro interno do servidor"));
+        }
+    }
+
+    @PutMapping(path = "atualizarNome/{id}")
+    public ResponseEntity<?> updateNameAndDescriptionImage(@PathVariable("id") UUID id,@RequestBody AttTitleAndDescriptionRequest request){
+        try{
+            Optional<Imagem> OptImagem = imagemRepository.findById(id);
+
+            if (OptImagem.isEmpty()){
+                return ResponseEntity.badRequest().body(new ErrorResponse("Imagem não encontrada!"));
+            }
+
+
+            Imagem imagem = OptImagem.get();
+
+            if (Boolean.TRUE.equals(imagem.getDeleted())) {
+                return ResponseEntity.badRequest()
+                        .body(new ErrorResponse("Imagem está deletada"));
+            }
+
+            imagem.setTitle(request.title());
+            imagem.setDescription(request.description());
+            Imagem updatedImagem = imagemRepository.save(imagem);
+
+            return  ResponseEntity.status(HttpStatus.OK).body(updatedImagem);
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("Erro interno do servidor"));
+        }
+    }
+
+    @PutMapping(path = "atualizarFile/{id}")
+    public ResponseEntity<?> updateFileImage(@PathVariable("id") UUID id,@RequestParam("file") MultipartFile file){
+        try{
+            Optional<Imagem> OptImagem = imagemRepository.findById(id);
+
+            if (OptImagem.isEmpty()){
+                return ResponseEntity.badRequest().body(new ErrorResponse("Imagem não encontrada!"));
+            }
+            if (!Objects.requireNonNull(file.getContentType()).startsWith("image")) {
+                return ResponseEntity.badRequest()
+                        .body(new ErrorResponse("Arquivo não é uma imagem!"));
+            }
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+            Path uploadDir = Paths.get("/app/uploads");
+
+            Path filePath = uploadDir.resolve(fileName);
+            Files.write(filePath, file.getBytes());
+
+            Imagem imagem = OptImagem.get();
+
+            if (Boolean.TRUE.equals(imagem.getDeleted())) {
+                return ResponseEntity.badRequest()
+                        .body(new ErrorResponse("Imagem está deletada"));
+            }
+
+            imagem.setPath(filePath.toString());
+            imagem.setType(file.getContentType());
+            imagem.setSize(file.getSize());
+            imagemRepository.save(imagem);
+
+            return  ResponseEntity.status(HttpStatus.OK).body(imagem);
+
         }catch(Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ErrorResponse("Erro interno do servidor"));
